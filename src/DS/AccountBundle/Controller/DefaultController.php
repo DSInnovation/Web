c9d2ffe2d4f2aabdaa8753ee1429e0fdf38e13b2
smile59 <17crazysak59@gmail.com>
2014-12-22 16:31:53 +0100

    connexion base

--- src/DS/AccountBundle/Controller/DefaultController.php
+++ src/DS/AccountBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace DS\AccountBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
@@ -12,7 +13,7 @@ class DefaultController extends Controller
      * @Route("/connexion")
      * @Template()
      */
-    public function connexionAction()
+    public function connexionAction(Request $request)
     {
         $form = $this->createFormBuilder()
                 ->add('login', 'text', array(
