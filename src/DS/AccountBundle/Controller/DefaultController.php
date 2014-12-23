cbe0648f96f2e76698a36e8e831cad631e77da60
smile59 <17crazysak59@gmail.com>
2014-12-23 15:27:27 +0100

    Better controller managing

--- src/DS/AccountBundle/Controller/DefaultController.php
+++ src/DS/AccountBundle/Controller/DefaultController.php
@@ -9,61 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\Session;
 
 class DefaultController extends Controller
-{
-    /**
-     * Create connexion form
-     * @return form
-     */
-    private function createFormConnexion()
-    {
-        $form = $this->createFormBuilder()
-                ->add('login', 'text', array(
-                    'label' => 'Nom de compte : '
-                ))
-                ->add('password', 'password', array (
-                    'label' => 'Mot de passe : '
-                ))
-                ->add('submit', 'submit', array (
-                    'label' => 'Se connecter'
-                ))
-                ->getForm();
-        
-        return $form;
-    }
-    
-    /**
-     * @Route(path = "/connexion", name="connexion")
-     * @Template()
-     */
-    public function connexionAction(Request $request)
-    {
-        $form = $this->createFormConnexion();
-
-        if(!$request->isMethod('post')) {
-            return array('form' => $form->createView());
-        }
-        
-        $form->bind($request);
-
-        if($form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $repository = $em->getRepository('DSAccountBundle:webAccount');
-
-            $user = $repository->findBy(
-                        array(
-                            'login' => $form->get('login')->getData(),
-                            'password' => $form->get('password')->getData()
-                        ));
-
-            if(1 == count($user)) {
-                $session = $request->getSession();
-                $session->set('id', $user[0]->getId());
-                return $this->redirect($this->generateUrl('index'));
-            }
-        }
-        return array('form' => $form->createView());
-    }
-    
+{    
     /**
      * @Route(path = "/menu", name="menu")
      * @Template()
--- src/DS/AccountBundle/Controller/InscriptionController.php
+++ src/DS/AccountBundle/Controller/InscriptionController.php
@@ -5,7 +5,7 @@ namespace DS\AccountBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Request;
 
 class InscriptionController extends Controller
 {
