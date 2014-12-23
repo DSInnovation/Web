8e245ade4704bd646065343fe4850c12b992b6fc
Quentin Buirette <quentin.buirette@epsi.fr>
2014-12-23 19:42:36 +0100

    Create Account Profil

--- src/DS/AccountBundle/Controller/AccountController.php
+++ src/DS/AccountBundle/Controller/AccountController.php
@@ -10,6 +10,28 @@ use Symfony\Component\HttpFoundation\Session\Session;
 class AccountController extends Controller
 {
     /**
+     * 
+     * 
+     */
+    public function jointureAction()
+    {
+        $session = new Session();
+    
+        $em = $this->getDoctrine()->getManager();
+
+        $query = $em->createQuery('
+            SELECT A.login, A.password, C.prenom, C.sexe, C.email, C.DateNaissance, F.nom, F.adresse, F.points
+            FROM DSAccountBundle:webAccount A 
+            INNER JOIN DSAccountBundle:appClient C ON A.idClient = C.id
+            INNER JOIN DSAccountBundle:appFamille F ON C.IdFamille = F.id;
+        ');
+
+   $query->setParameter('id', $session->get('id'));
+
+        return $query->getResult();
+    }
+
+        /**
      * @Route(path="/compte", name="moncompte")
      * @Template()
      */
@@ -20,13 +42,13 @@ class AccountController extends Controller
         
         if(!$session->get('id')) {
             return $this->redirect ($this->generateUrl ("connexion"));
+        } else {
+            return $this->redirect ($this->generateUrl ("moncompte"));
         }
+       
         
-        $var = $this->getDoctrine()->getManager();
-        $info = $var->getRepository('DSAccountBundle:webAccount');
-        
-        $infos = $info->findOneBy(array('id' => $session->get('id')));
-
+        $infos = $this->jointureAction();
+   //     var_dump($infos);
         return array('infoaccount' => $infos);
     }
 }
