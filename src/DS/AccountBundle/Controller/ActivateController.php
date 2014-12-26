e7c43622a802188ceb204545d168fa7e7f8d6afc
Quentin Buirette <quentin.buirette@epsi.fr>
2014-12-26 17:34:52 +0100

    Modify gitignore and create activateController for email validation

---
+++ src/DS/AccountBundle/Controller/ActivateController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace DS\AccountBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Session\Session;
+
+class ActivateController extends Controller
+{
+    
+}
