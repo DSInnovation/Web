cbd0e56daef4087f06d9f81e031a11803384d971
Quentin Buirette <quentin.buirette@epsi.fr>
2014-12-26 19:17:05 +0100

    Correction routing error that makes website down

--- src/DS/AccountBundle/Controller/ActivateController.php
+++ src/DS/AccountBundle/Controller/ActivateController.php
@@ -7,12 +7,5 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class ActivateController extends Controller
 {
-    /**
-     * @Route(path"/account/validate/{url}", name="validate")
-     */
     
-    public function validateAction($url)
-    {
-        
-    }
 }
