ed98c4ac6149e670a61778a717be5356dd34fcc4
Quentin Buirette <quentin.buirette@epsi.fr>
2014-12-26 19:34:31 +0100

    Modify view inscription, the div is now top right

--- src/DS/AccountBundle/Controller/ActivateController.php
+++ src/DS/AccountBundle/Controller/ActivateController.php
@@ -7,5 +7,12 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class ActivateController extends Controller
 {
+    /**
+     * @Route(path"/account/validate/{url}", name="validate")
+     
     
+    public function validateAction()
+    {
+        
+    } */
 }
